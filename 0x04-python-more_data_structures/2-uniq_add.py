6cb482b00a65b92b2c076e804a8a5cc6d542e5bb
ibrahimabdusurur <117864920+ibrahimabdusurur@users.noreply.github.com>
2023-04-26 07:33:56 -0700

    1 and 2 made executable

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
+
 def uniq_add(my_list=[]):
-    """Add all unique integers in a list (once for each integer)."""
-    result = 0
-    for x in set(my_list):
-        result += x
-    return (result)
+    add = 0
+    return ((add + my_list[i]) for i in range(len(my_list)))
