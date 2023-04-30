58ab93310454b840ac01527b73f7a8858b6df220
ibrahimabdusurur <117864920+ibrahimabdusurur@users.noreply.github.com>
2023-04-30 06:03:30 -0700

    6-print_sorted_dictionary.py - string format changed

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
+
 def print_sorted_dictionary(a_dictionary):
-    """Print a dictionary by ordered keys."""
-    [print("{}: {}".format(k, a_dictionary[k])) for k in sorted(a_dictionary)]
+    [print(f"{v}: {a_dictionary[v]}") for v in sorted(a_dictionary)]
