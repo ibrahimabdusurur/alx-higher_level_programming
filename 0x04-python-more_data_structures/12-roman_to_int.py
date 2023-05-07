861edb24d618d715acb5967c2a2b933747ac8309
ibrahimabdusurur <117864920+ibrahimabdusurur@users.noreply.github.com>
2023-05-07 09:21:45 -0700

    12-roman_to_int.py for loop modified

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
+
 def roman_to_int(roman_string):
-    """Converts a roman numeral to an integer."""
-    if (not isinstance(roman_string, str) or
-            roman_string is None):
-        return (0)
+    if not isinstance(roman_string, str) or len(roman_string) == 0:
+        return 0
 
     roman_dict = {
             "I": 1,
@@ -14,16 +13,16 @@ def roman_to_int(roman_string):
             "D": 500,
             "M": 1000
     }
-    num = 0
 
+    decimal = 0
     for i in range(len(roman_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
-            return (0)
+        if (i != (len(roman_string) - 1) and roman_dict[roman_string[i]] >=
+                roman_dict[roman_string[i + 1]]):
+            decimal += roman_dict[roman_string[i]]
+        elif i == (len(roman_string) - 1):
+            decimal += roman_dict[roman_string[i]]
+        elif (i != (len(roman_string) - 1) and roman_dict[roman_string[i]] <
+                roman_dict[roman_string[i + 1]]):
+            decimal -= roman_dict[roman_string[i]]
 
-        if (i != (len(roman_string) - 1) and
-                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
-
-        else:
-            num += roman_dict[roman_string[i]]
-    return (num)
+    return decimal
